23f7cd7f324de1bb84319b047a01edde4fcac96e
Atp1704 <abhay@timepilot.tech>
2025-01-05 20:15:00 +0530

    tgw

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit
           <code>src/App.js</code>
-          and save to reloads.
+          and save to trigger.
         </p>
         <a
           className="App-link"
