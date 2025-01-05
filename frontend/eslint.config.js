34ef5edc42c3b27f48011dc186ada9c4c01cdfff
Atp1704 <abhay@timepilot.tech>
2025-01-05 22:07:46 +0530

    tgw

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  extends: ['airbnb', 'plugin:react/recommended'],
-  env: {
-    browser: true,
+import airbnb from 'eslint-config-airbnb';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  airbnb,
+  {
+    env: {
+      browser: true,
+    },
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      'react/jsx-props-no-spreading': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-  rules: {
-    'react/jsx-props-no-spreading': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-};
+];
